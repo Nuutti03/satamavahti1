877980377bdd63c5198ed00764bd988e10b159d9
Nuutti03 <nuutti.roiha@student.lab.fi>
2025-04-01 15:18:54 +0300

    push

---
+++ hakukentta/hakukentta.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="Satamavahti.WebForm" %>
+
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Satamavahti</title>
+    <link rel="stylesheet" href="StyleSheet1.css">
+</head>
+<body>
+    <h1>Satamavahti</h1>
+    <form id="satamaForm" runat="server" method="post">
+        <label for="satama">Valitse satama:</label>
+        <select id="satama" name="satama" runat="server">
+            <option value="hanko">Hanko</option>
+            <option value="helsinki">Helsinki</option>
+            <option value="inkoo">Inkoo</option>
+            <option value="kotka-hamina">Kotka-Hamina</option>
+            <option value="loviisa">Loviisa</option>
+            <option value="sköldvik">Sköldvik</option>
+        </select>
+        <br><br>
+
+        <label for="laiva">Valitse laiva:</label>
+        <select id="laiva" name="laiva" runat="server">
+            <option value="purjelaiva">Purjelaiva</option>
+            <option value="soutuvene">Soutuvene</option>
+            <option value="moottorivene">Moottorivene</option>
+            <option value="kanootti">Kanootti</option>
+        </select>
+        <br><br>
+
+        <button type="submit" id="Button1">Näytä tiedot</button>
+    </form>
+
+    <div id="laivaTiedot">
+        <strong>Valitsemasi tiedot:</strong>
+        <p id="tulos"><%= Tiedot %></p>
+    </div>
+</body>
+</html>
---
+++ hakukentta/hakukentta.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Satamavahti
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        // Tämä voi olla esimerkki tiedoista, joita voisi tulla tietokannasta.
+        private static readonly string[] satamat = { "Hanko", "Helsinki", "Inkoo", "Kotka-Hamina", "Loviisa", "Sköldvik" };
+        private static readonly string[] laivat = { "Purjelaiva", "Soutuvene", "Moottorivene", "Kanootti" };
+
+        // Tiedot aluksesta
+        private static string[] alustenTiedot =
+        {
+            "Purjelaiva: 12 metriä pitkä, 3 metriä leveä, nopeus 10 solmua.",
+            "Soutuvene: 4 metriä pitkä, 1.2 metriä leveä, nopeus 5 solmua.",
+            "Moottorivene: 8 metriä pitkä, 2.5 metriä leveä, nopeus 30 solmua.",
+            "Kanootti: 5 metriä pitkä, 1 metri leveä, nopeus 3 solmua."
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Tarkistetaan, onko lomake lähetetty
+            if (IsPostBack)
+            {
+                // Haetaan valitut tiedot
+                string valittuSatama = Request.Form["satama"];
+                string valittuLaiva = Request.Form["laiva"];
+
+                // Haetaan aluksen tiedot valitun aluksen perusteella
+                string laivanTiedot = "";
+
+                switch (valittuLaiva)
+                {
+                    case "purjelaiva":
+                        laivanTiedot = alustenTiedot[0];
+                        break;
+                    case "soutuvene":
+                        laivanTiedot = alustenTiedot[1];
+                        break;
+                    case "moottorivene":
+                        laivanTiedot = alustenTiedot[2];
+                        break;
+                    case "kanootti":
+                        laivanTiedot = alustenTiedot[3];
+                        break;
+                }
+
+                // Näytetään tiedot
+                Tiedot = $"Satama: {valittuSatama}<br>Laiva: {valittuLaiva}<br>Tiedot: {laivanTiedot}";
+            }
+        }
+
+        public string Tiedot { get; set; }
+    }
+}
